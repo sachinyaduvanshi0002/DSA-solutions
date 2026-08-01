8b0d536f2e77bca081efee7931f41af429e008d2
SACHIN KUMAR <sachinkumar30062005@gmail.com>
2026-08-01 08:26:19 +0530

    Delete LeetCode directory

--- LeetCode/Python/Easy/70. Climbing Stairs/Solution.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def climbStairs(self, n):
-        if n <= 2 : return n
-        dp = [0] * (n+1)
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
