e5d59dc8c25693a02ed822056c39a0a323ffdf78
SACHIN KUMAR <sachinkumar30062005@gmail.com>
2026-08-01 08:12:50 +0530

    Time: 3 ms (56.65%), Space: 12.9 MB (9.25%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,14 +1,19 @@
 class Solution(object):
     def predictTheWinner(self, nums):
         n = len(nums)
-        i = 0
-        j = n-1
-        def dfs(i, j):
-            if i == j:
-                return nums[i]
-            
-            takei = nums[i] - dfs(i+1, j)
-            takej = nums[j] - dfs(i, j-1)
+        memo = {}
 
-            return max(takei, takej)
-        return dfs(i, j) >= 0
+        def dfs(l, r):
+            if l == r:
+                return nums[l]
+            
+            if (l,r) in memo:
+                return memo[(l,r)]
+
+            takel = nums[l] - dfs(l+1, r)
+            taker = nums[r] - dfs(l, r-1)
+
+            memo[(l,r)] = max(takel, taker)
+            return memo[(l,r)]
+
+        return dfs(0, n-1) >= 0
