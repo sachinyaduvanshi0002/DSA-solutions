170e0d4c5ee700cc0cbc91927810bd4e3fd66613
SACHIN KUMAR <sachinkumar30062005@gmail.com>
2026-08-13 07:45:15 +0530

    Time: 586 ms (36.67%), Space: 12.4 MB (53.83%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def fib(self, n):
+        def fibo(n):
+            if n == 0: return 0
+            elif n == 1: return 1
+            else: 
+                return fibo(n-1) + fibo(n-2)
+        
+        return fibo(n)
