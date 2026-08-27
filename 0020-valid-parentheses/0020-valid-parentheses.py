a3717dfbc3bdf8a982e076d2acfb42d56d062842
SACHIN KUMAR <sachinkumar30062005@gmail.com>
2026-08-27 09:14:28 +0530

    Time: 1 ms (86.29%), Space: 12.4 MB (72.26%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,24 +1,38 @@
 class Solution(object):
     def isValid(self, s):
-        stack = []
-        for ch in s:
+        # method-1:
 
-            if ch in "([{":
-                stack.append(ch)
+        # stack = []
+        # for ch in s:
+
+        #     if ch in "([{":
+        #         stack.append(ch)
             
-            else:
-                if not stack:
+        #     else:
+        #         if not stack:
+        #             return False
+
+        #         top = stack.pop()
+
+        #         if ch == ")" and top != "(":
+        #             return False
+
+        #         elif ch == "]" and top != "[":
+        #             return False
+
+        #         elif ch == "}" and top != "{":
+        #             return False
+
+        # return len(stack) == 0
+
+        # Method-2:
+
+        stack = []
+        brackets = {')':'(', ']':'[', '}':'{'}
+        for ch in s:
+            if ch in brackets:
+                if not stack or stack[-1] != brackets[ch]:
                     return False
-
-                top = stack.pop()
-
-                if ch == ")" and top != "(":
-                    return False
-
-                elif ch == "]" and top != "[":
-                    return False
-
-                elif ch == "}" and top != "{":
-                    return False
-
+                stack.pop()
+            else: stack.append(ch)
         return len(stack) == 0
