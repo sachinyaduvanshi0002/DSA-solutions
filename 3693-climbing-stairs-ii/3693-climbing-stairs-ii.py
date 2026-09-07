eb06f8be001cfbbaf0ca758ee97ab89b45f2fc61
SACHIN KUMAR <sachinkumar30062005@gmail.com>
2026-09-07 08:08:32 +0530

    Time: 192 ms (100%), Space: 20.7 MB (99.03%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.py
@@ -1,17 +1,7 @@
 class Solution(object):
     def climbStairs(self, n, costs):
-        dp = [0] * (n+1)
-        dp[0] = 0
-        if n >= 1:
-            dp[1] = dp[0] + costs[0] + (1-0)**2
-        if n>=2:
-            dp[2] = min(
-                dp[1] + costs[1] + (2-1)**2,
-                dp[0] + costs[1] + (2-0)**2
-        )
-        for i in range(3, n+1):
-            dp[i] = min(
-                dp[i-1]+costs[i-1]+(i-(i-1))**2,
-                dp[i-2]+costs[i-1]+(i-(i-2))**2,
-                dp[i-3]+costs[i-1]+(i-(i-3))**2)
-        return dp[n]
+
+        v0 = v1 = v2 = 0
+        for c in costs:
+            v0,v1,v2 = v1, v2, min(v0+9, v1+4, v2+1) + c
+        return v2
