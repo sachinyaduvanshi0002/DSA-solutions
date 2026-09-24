f7485dfcf6dd51f270375c67062e52ecc17f71a2
SACHIN KUMAR <sachinkumar30062005@gmail.com>
2026-09-24 10:08:46 +0530

    Time: 12 ms (23.91%), Space: 12.5 MB (21.74%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def smallestIndex(self, nums):
-        idx = float('inf')
         for i in range(len(nums)):
             add = 0
             while nums[i] != 0:
@@ -9,8 +8,6 @@ class Solution(object):
                 nums[i] //= 10
             
             if add == i:
-                idx = min(idx, i)
+                return i
 
-        if idx != float('inf'):
-            return idx
-        else: return -1
+        return -1
